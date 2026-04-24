85c12f2127e3e1be421d39ef37c2d138b4eeed48
hemrajsharmabhl-lab <hemrajsharma.bhl@gmail.com>
2026-04-24 18:25:16 +0530

    240420260630PM
    SS

--- RelianceProj/NewDevelopment/SqlDBMenudesign.vb
+++ RelianceProj/NewDevelopment/SqlDBMenudesign.vb
@@ -7,10 +7,7 @@ Public Class SqlDBMenudesign
     Public DataMenuName As DataTable
     Public DataMstUser As DataTable
     Public InsertDataMenuNameTable As DataTable
-    'Dim SaveQuery As String = ""
-    'Private tblFormValues As New DataTable
-    'Private _FORMMODE As String = ""
-    'Private FieldNameAndValues(1) As String
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'MenuTable Data
         RS = "SELECT * FROM MenuTable WHERE 1=1 ORDER BY Id"
@@ -48,14 +45,30 @@ Public Class SqlDBMenudesign
         Dim FnlTbl As DataTable
         FnlTbl = DataMenuName.Clone
         'menu master data
-        For Each dr As DataRow In Datamenutable.Select()
-            For Each dr1 As DataRow In DataMenuName.Select("(MainId='" & dr("ID") & "' OR MenuPositionId='" & dr("ID") & "') AND ActiveStatus='YES'")
-                Dim exists As Boolean = FnlTbl.Select("ID='" & dr1("ID") & "'").Length > 0
-                If exists = False Then
-                    FnlTbl.ImportRow(dr1)
-                End If
-            Next
+        'For Each dr As DataRow In Datamenutable.Select()
+        '    For Each dr1 As DataRow In DataMenuName.Select("(MainId='" & dr("ID") & "' OR MenuPositionId='" & dr("ID") & "') AND ActiveStatus='YES'")
+        '        Dim exists As Boolean = FnlTbl.Select("ID='" & dr1("ID") & "'").Length > 0
+        '        If exists = False Then
+        '            FnlTbl.ImportRow(dr1)
+        '        End If
+        '    Next
+        'Next
+
+        For Each dr1 As DataRow In DataMenuName.Select("ActiveStatus='YES'")
+
+            Dim isMatch As Boolean = Datamenutable.Select("ID='" & dr1("MainId") & "' OR SUBID='" & dr1("MenuPositionId") & "'").Length > 0
+            If Datamenutable.Rows.Count > 0 AndAlso Datamenutable.Rows(0)("OP10").ToString().Trim() = "New Menu" Then
+                Exit Sub
+            End If
+            Dim exists As Boolean = FnlTbl.Select("ID='" & dr1("ID") & "'").Length > 0
+            If isMatch = False AndAlso exists = False Then
+                'FnlTbl.ImportRow(dr1)
+                Dim _ActiveStatus As String = If(dr1("ActiveStatus").ToString().Trim().ToUpper() = "YES", "Y", "N")
+                RS = "Insert into MenuTable(MenuId,Menu,SUBID,ORDERNO,Active_Status,OP10) Values(" & dr1("MainId") & ",'" & dr1("MenuName") & "','" & dr1("MenuPositionId") & "'," & dr1("MenuOrderNo") & ",'" & _ActiveStatus & "','New Menu')"
+                SQLDBMENU_Save_Delete_Update()
+            End If
         Next
+
         'user master data
         Dim Fnlmenutbl As DataTable
         Fnlmenutbl = DatauserMenu.Clone
@@ -64,20 +77,25 @@ Public Class SqlDBMenudesign
                 Fnlmenutbl.ImportRow(dr1)
             Next
         Next
-        '_FORMMODE = "ADD"
-        'ObjCls_General._InsertFormValueIntoDataTable(Me, tblFormValues)
-        'ObjCls_General.MAKEQUERYFROMDATATABLE(_FORMMODE, tblFormValues, FieldNameAndValues)
-        'SaveQuery = getSaveQuery()
-        'RS = SaveQuery.ToString
-        'MenuDesign_QuerySaveUpdateDelete()
+
+        Dim _USerWIseMEnuTbl As New DataTable
+        _USerWIseMEnuTbl = DataMenuName.Clone
+        Dim startSubMenuId As Integer = 8
+        Dim endSubMenuId As Integer = 23
+        For Each dr As DataRow In DataMstUser.Select()
+            'Har USER_ID ke liye _submenuid ko 8 se start karo
+            For _submenuid As Integer = startSubMenuId To endSubMenuId
+                For Each dr1 As DataRow In DatauserMenu.Select("USERID='" & dr("USER_ID") & "'")
+                    If dr1("MENUID") = _submenuid Then
+                        For Each dr2 As DataRow In DataMenuName.Select("MenuPositionId='" & _submenuid & "' and ActiveStatus='YES'")
+                            '_USerWIseMEnuTbl.ImportRow(dr2)
+                            Dim _ActiveStatus As String = If(dr2("ActiveStatus").ToString().Trim().ToUpper() = "YES", "Y", "N")
+                            RS = "Insert into UserMenu(menuId,userId,Active_Status) Values(" & _submenuid & "," & dr("USER_ID") & ",'" & _ActiveStatus & "')"
+                            SQLDBMENU_Save_Delete_Update()
+                        Next
+                    End If
+                Next
+            Next
+        Next
     End Sub
-    'Private Function getSaveQuery()
-    '    _strQuery = New StringBuilder
-    '    If _FORMMODE = "ADD" Then
-    '        _strQuery.Append(" INSERT INTO " & _TblName & "(" & FieldNameAndValues(0) & ")  VALUES  (" & FieldNameAndValues(1) & ")")
-    '    ElseIf _FORMMODE = "EDIT" Then
-    '        _strQuery.Append(" UPDATE " & _TblName & " SET " & FieldNameAndValues(1) & " WHERE " & _KeyFieldName & "=" & "" & _KeyFieldValue & "")
-    '    End If
-    '    getSaveQuery = _strQuery.ToString
-    'End Function
 End Class
